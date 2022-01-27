a596d80dbd80c7dfea9b069370525f90f1a19870
Pandor82 <84559611+Pandor82@users.noreply.github.com>
2022-01-28 00:43:57 +0200

    Add files via upload

---
+++ Megabit/index.php
@@ -0,0 +1,68 @@
+﻿
+<html lang="en">
+<head>
+    <meta charset="utf-8" />
+    <title>Meganit</title>
+    <script src="https://kit.fontawesome.com/a076d05399.js" crossorigin="anonymous"></script>
+    <link rel="stylesheet" href="defaults.css">
+</head>
+
+<body >
+    <div class="image">
+    </div>
+
+    <div class="TOPBAR">
+        <div class="LOGO_PINEAPPLE">
+            <img src="Image_File/Union.jpg" alt="Pineapple"  
+            style= " position: absolute; width: 25px; height: 40px; left: 100px; top: 100px;">
+            <img src="Image_File/pineapple.png" alt="Pineapple" 
+            style="position: absolute; width: 78px;height: 16px;left: 140px;top: 120px;">
+        </div>
+        <a href="#" class="NavBar" id="About">About </a>
+        <a href="#" class="NavBar" id="How_it_works">How it works</a>
+        <a href="#" class="NavBar" id="Contact">Contact</a>
+    </div>
+
+    <h2 id="Newsletter">Subscribe to newsletter</h2>
+    <h4 id="Info">Subscribe to our newsletter and get 10% discount on pineapple glasses.</h4>
+
+    
+        <form method="post">
+        <input type="text" id="EMAIL_INPUT" name="Email" placeholder="Type your email address here…" >
+        <input type="button" class="button">
+        </form>
+    
+
+    
+        <div class="ic_check">
+          <div class="layer1"></div>
+          <div class="layer2"></div>
+          <div class="terms_of_service">
+          <label > I agree to <a href="#">term of service</a></label><br>
+          </div>
+        </div>
+
+
+    <hr style=" position: absolute; width: 400px; height: 1px; left: 140px; top: 740px;">
+
+    <div class="SOCIAL">
+          <div class="FACEBOOK_layer1"></div>
+          <div class="FACEBOOK_layer2"></div>
+    </div>
+
+    <div class="INSTAGRAM">
+          <div class="INSTAGRAM_layer1"></div>
+          <div class="INSTAGRAM_layer2"></div>
+    </div>
+
+   
+          <div class="TWITTER_layer1"></div>
+          <div class="TWITTER_layer2"></div>
+    
+    
+          <div class="INSTAGRAM_layer1"></div>
+          <div class="INSTAGRAM_layer2"></div>
+    </div> 
+
+</body>
+</html>
